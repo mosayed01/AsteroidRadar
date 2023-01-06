96d5e177ae7ad46815f144ee65ae9143d5fcaf58
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-01-07 00:20:31 +0200

    test worker

--- app/src/main/java/com/udacity/asteroidradar/BaseApp.kt
+++ app/src/main/java/com/udacity/asteroidradar/BaseApp.kt
@@ -1,12 +1,23 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
+import android.os.Build
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.udacity.asteroidradar.data.local.AsteroidDB
+import com.udacity.asteroidradar.domain.worker.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class BaseApp : Application() {
-
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     @InternalCoroutinesApi
     override fun onCreate() {
@@ -15,5 +26,34 @@ class BaseApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree());
         }
+        initWorker()
+    }
+
+    private fun initWorker() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
